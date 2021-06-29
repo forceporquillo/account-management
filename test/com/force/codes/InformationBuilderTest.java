7e5953f6b20b5fa4536c36241970613a6cc8c598
strongforce1 <201811941@fit.edu.ph>
2021-06-29 08:45:00 +0800

    Create InformationBuilderTest.java

---
+++ test/com/force/codes/InformationBuilderTest.java
@@ -0,0 +1,57 @@
+package com.force.codes;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * This is just a basic test to test out constructing information using builder design pattern.
+ */
+
+public class InformationBuilderTest extends BaseTestCase {
+
+    private Information information;
+
+
+    @Test
+    @Before
+    public void createInfoInstance() {
+        information = createBuilderInstance();
+    }
+
+    @Test
+    public void test_get_student_id() {
+        assertEquals(information.getStudentId(), STUDENT_ID);
+    }
+
+    @Test
+    public void test_get_name() {
+        assertEquals(information.getName(), NAME);
+    }
+
+    @Test
+    public void test_get_course() {
+        assertEquals(information.getCourse(), COURSE);
+    }
+
+    @Test
+    public void test_get_year_level() {
+        assertEquals(information.getYearLevel(), YEAR);
+    }
+
+    @Test
+    public void test_get_address() {
+        assertEquals(information.getAddress(), ADDRESS);
+    }
+
+    @Test
+    public void test_get_email_address() {
+        assertEquals(information.getEmail(), EMAIL);
+    }
+
+    @Test
+    public void test_get_phone_number() {
+        assertEquals(information.getPhoneNumber(), CONTACT);
+    }
+}
