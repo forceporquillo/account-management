15be1117c6cc4cf064530b09235f46cad3c1fce8
strongforce1 <201811941@fit.edu.ph>
2021-06-29 08:45:21 +0800

    Create Constants.java

---
+++ src/com/force/codes/Constants.java
@@ -0,0 +1,23 @@
+package com.force.codes;
+
+public class Constants {
+
+    public static final String INVALID_STUDENT_ID       = "Invalid Student ID";
+    public static final String INVALID_PHONE            = "Invalid Phone Number";
+    public static final String INVALID_EMAIL            = "Invalid Email Address";
+    public static final String INVALID_ADDRESS          = "Invalid Address";
+
+    public static final String ERROR_EMPTY              = "Field must not be empty";
+
+    public static final String ERROR                    = "Error";
+
+    public static final String ERROR_BAD_FORMAT         = "Email is badly formatted.";
+
+    public static final String MESSAGE_SUCCESS          = "Information Successfully Added!";
+
+    public static final String SUCCESS                  = "Success";
+
+    public static final String UPDATE_SUCCESS           = "Account Update Success";
+
+    public static final String MESSAGE_UPDATED          = "Information Successfully Updated!";
+}
