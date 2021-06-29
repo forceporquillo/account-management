580ebc5336743e747aba275cb44fbfdf9fbe50ec
strongforce1 <201811941@fit.edu.ph>
2021-06-29 08:45:11 +0800

    Create Util.java

---
+++ src/com/force/codes/Util.java
@@ -0,0 +1,60 @@
+package com.force.codes;
+
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Util {
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$";
+
+    public static boolean isValid(String email) {
+        if (email == null)
+            return false;
+
+        return Pattern.compile(EMAIL_REGEX)
+                .matcher(email)
+                .matches();
+    }
+
+    public static String[] objectToArray(Information information) {
+        return new String[]{
+                information.getStudentId(),
+                information.getName(),
+                information.getCourse(),
+                String.valueOf(information.getYearLevel()),
+                information.getAddress(),
+                information.getEmail(),
+                information.getPhoneNumber()
+        };
+    }
+
+    public static Information updateInfo(Information newInfo, Information oldInfo) {
+        oldInfo.setStudentId(newInfo.getStudentId());
+        oldInfo.setName(newInfo.getName());
+        oldInfo.setCourse(newInfo.getCourse());
+        oldInfo.setYearLevel(newInfo.getYearLevel());
+        oldInfo.setAddress(newInfo.getAddress());
+        oldInfo.setEmail(newInfo.getEmail());
+        oldInfo.setPhoneNumber(newInfo.getPhoneNumber());
+
+        return oldInfo;
+    }
+
+    public static String splitStream(String string) {
+        UnaryOperator<String> capitalize = str ->
+                str.substring(0, 1).toUpperCase() +
+                        str.substring(1).toLowerCase();
+
+        return Stream.of(
+                string.split(" ")
+        ).map(capitalize)
+                .collect(
+                        Collectors.joining(" ")
+                );
+    }
+}
