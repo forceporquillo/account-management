287b49b1631060f09186ecc9c7e4ff7281f727a3
strongforce1 <201811941@fit.edu.ph>
2021-06-29 08:45:28 +0800

    Create AccountManagement.java

---
+++ src/com/force/codes/AccountManagement.java
@@ -0,0 +1,14 @@
+package com.force.codes;
+
+public class AccountManagement {
+
+    public static void main(String[] args) {
+        final AccountManagerStateImpl state = AccountManagerStateImpl.getInstance();
+
+        create(state);
+    }
+
+    static void create(AccountManagerState state) {
+
+    }
+}
