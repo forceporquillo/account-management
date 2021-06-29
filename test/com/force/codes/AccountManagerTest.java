e1d6bd034abea9741ade617dc59a1c7b0419315b
strongforce1 <201811941@fit.edu.ph>
2021-06-29 08:45:07 +0800

    Create AccountManagerTest.java

---
+++ test/com/force/codes/AccountManagerTest.java
@@ -0,0 +1,27 @@
+package com.force.codes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountManagerTest {
+
+    @Before
+    @Test
+    public void valid_email() {
+        String email = "force@gmail.com";
+        Assert.assertTrue(Util.isValid(email));
+    }
+
+    @Test
+    public void invalid_email() {
+        String email = "force@gmail.com12313asdasd";
+        Assert.assertFalse(Util.isValid(email));
+    }
+
+    @Test
+    public void must_upper_case_per_space() {
+        String test = "this is a test";
+        Assert.assertEquals("This Is A Test", Util.splitStream(test));
+    }
+}
